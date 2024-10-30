0c11cf230ecfcda81f76ac9ee5f0472a6c049e46
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-10-30 09:49:36 +0300

    return to ssc

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,23 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Results from "./components/results";
-import { useSearchParams } from "next/navigation";
+const API_KEY = process.env.API_KEY;
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export default async function Page({ searchParams }) {
+  const searchParam = await searchParams;
+  const genre = searchParam.genre || "fetchTrending";
 
-export default function Page() {
-  const [results, setResults] = useState([]);
-  const [error, setError] = useState(null);
-  const searchParams = useSearchParams(); // Use this hook to access searchParams
+  try {
+    const resp = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
+    );
 
-  useEffect(() => {
-    const genre = searchParams.get("genre") || "fetchTrending"; // Unwrap genre safely
-
-    const fetchData = async () => {
-      try {
-        const resp = await fetch(
-          `https://api.themoviedb.org/3${
-            genre === "fetchTopRated"
-              ? `/movie/top_rated`
-              : `/trending/all/week`
-          }?api_key=${API_KEY}&language=en-US&page=1`
-        );
-        if (!resp.ok) throw new Error("Failed to fetch data");
-
-        const data = await resp.json();
-        setResults(data.results);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-        setError("Error loading data");
-      }
-    };
-
-    fetchData();
-  }, [searchParams]); // Depend on searchParams, so it re-fetches if genre changes
-
-  if (error) {
-    return <div>{error}</div>;
+    if (!resp.ok) throw new Error("Failed to fetch data");
+    const data = await resp.json();
+    return <Results results={data.results} />;
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
   }
-
-  return <Results results={results} />;
 }
