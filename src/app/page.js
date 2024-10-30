3723eb8de320a537be75566624c0b5624c358c38
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-10-30 09:39:27 +0300

    convert main page into csc instead of ssc

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,44 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Results from "./components/results";
-const API_KEY = process.env.API_KEY;
+import { useSearchParams } from "next/navigation";
 
-export default async function Page({ searchParams }) {
-  const searchParam = await searchParams;
-  const genre = searchParam.genre || "fetchTrending";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-  try {
-    const resp = await fetch(
-      `https://api.themoviedb.org/3${
-        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-      }?api_key=${API_KEY}&language=en-US&page=1`,
-      { next: { revalidate: 10000 } }
-    );
+export default function Page() {
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const searchParams = useSearchParams(); // Use this hook to access searchParams
 
-    if (!resp.ok) throw new Error("Failed to fetch data");
-    const data = await resp.json();
-    return <Results results={data.results} />;
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
+  useEffect(() => {
+    const genre = searchParams.get("genre") || "fetchTrending"; // Unwrap genre safely
+
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(
+          `https://api.themoviedb.org/3${
+            genre === "fetchTopRated"
+              ? `/movie/top_rated`
+              : `/trending/all/week`
+          }?api_key=${API_KEY}&language=en-US&page=1`
+        );
+        if (!resp.ok) throw new Error("Failed to fetch data");
+
+        const data = await resp.json();
+        setResults(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+        setError("Error loading data");
+      }
+    };
+
+    fetchData();
+  }, [searchParams]); // Depend on searchParams, so it re-fetches if genre changes
+
+  if (error) {
+    return <div>{error}</div>;
   }
+
+  return <Results results={results} />;
 }
