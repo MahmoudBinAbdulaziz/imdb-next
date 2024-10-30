38da5be5edeb27b18bfa67a268731e5ff0dc8929
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-10-30 08:12:18 +0300

    fix bug

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,21 @@ import React from "react";
 import Results from "./components/results";
 const API_KEY = process.env.API_KEY;
 
-async function page({ searchParams }) {
+export default async function Page({ searchParams }) {
   const searchParams2 = await searchParams;
   const genre = searchParams2.genre || "fetchTrending";
 
-  const resp = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
-  const data = await resp.json();
-  if (!resp.ok) {
-    throw new Error("Failed to fetch data");
+  try {
+    const resp = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    if (!resp.ok) throw new Error("Failed to fetch data");
+    const data = await resp.json();
+    return <Results results={data.results} />;
+  } catch (error) {
+    console.error(error);
+    return <div>Error loading data</div>; // Provide a fallback
   }
-  const results = data.results;
-
-  return <Results results={results} />;
 }
-
-export default page;
