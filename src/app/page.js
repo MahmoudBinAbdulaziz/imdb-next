ed463eb8cf521be6745c380490e88269d969ef8c
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-10-30 09:27:13 +0300

    add revalidate

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,9 @@ export default async function Page({ searchParams }) {
       `https://api.themoviedb.org/3${
         genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
       }?api_key=${API_KEY}&language=en-US&page=1`,
-      { cache: "no-store" }
+      { next: { revalidate: 10000 } }
     );
+
     if (!resp.ok) throw new Error("Failed to fetch data");
     const data = await resp.json();
     return <Results results={data.results} />;
